e1afc799b6bd6edaf8dd30f2a00fd0d6f067a51c
John Pham <jdcpham@outlook.com>
2017-03-02 19:52:59 +0000

    New Package

--- ppa-major-coursework/src/requirementX/Main.java
+++ ppa-major-coursework/src/requirementX/Main.java
@@ -2,15 +2,17 @@ package requirementX;
 
 import api.ripley.Ripley;
 
+import view.UFOMainFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Ripley ripley = new Ripley("10tLI3CWstqyVD6ql2OMtA==", "tBgm4pRo9grVqL46EnH7ew==");
 		
-		System.out.print(ripley.getLastUpdated());
+		new UFOMainFrame().setVisible(true);
 		
-		
+
 
 	}
 
---
+++ ppa-major-coursework/src/view/UFOMainFrame.java
@@ -0,0 +1,23 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class UFOMainFrame extends JFrame{
+	
+	public UFOMainFrame() {
+		
+		
+		initWidgets();
+		
+	}
+	
+	
+	
+	public void initWidgets() {
+		
+		
+		pack();
+		
+	}
+
+}
