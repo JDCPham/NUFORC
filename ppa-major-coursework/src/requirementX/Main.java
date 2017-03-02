a4fa661bf3922968422be00e51b9369cf4d3487f
John Pham <k1630506@kcl.ac.uk>
2017-03-02 19:47:29 +0000

    New Package.

---
+++ ppa-major-coursework/src/requirementX/Main.java
@@ -0,0 +1,17 @@
+package requirementX;
+
+import api.ripley.Ripley;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Ripley ripley = new Ripley("10tLI3CWstqyVD6ql2OMtA==", "tBgm4pRo9grVqL46EnH7ew==");
+		
+		System.out.print(ripley.getLastUpdated());
+		
+		
+
+	}
+
+}
