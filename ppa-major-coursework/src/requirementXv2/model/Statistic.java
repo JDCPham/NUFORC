1036701e9a7493521820bf450fe8c46c0a85ba3c
John Pham <jdcpham@outlook.com>
2017-03-17 20:15:47 +0000

    Panel 2 Complete

---
+++ ppa-major-coursework/src/requirementXv2/model/Statistic.java
@@ -0,0 +1,23 @@
+package requirementXv2.model;
+
+public class Statistic {
+	
+	private String title;
+	private String stat;
+	
+	
+	/** Getters **/
+	
+	public String getTitle() { return title; }
+	
+	public String getStat() { return stat; }
+	
+	
+	
+	/** Setters **/
+	
+	public void setTitle(String title) { this.title = title; }
+	
+	public void setStat(String stat) { this.stat = stat; }
+
+}
