74a9d3805096243f25547ee6e1524e8c9cb06def
Emmanuel <Emmanuel@Manu>
2017-03-21 16:51:27 +0000

    Added photoshopped alien heads to the side panels.

--- ppa-major-coursework/src/requirementXv2/view/EscapeChatFrame.java
+++ ppa-major-coursework/src/requirementXv2/view/EscapeChatFrame.java
@@ -2,6 +2,7 @@ package requirementXv2.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -16,11 +17,21 @@ import javax.swing.JTextField;
 
 public class EscapeChatFrame extends JPanel implements Observer {
 
-	private EscapePicturePanel sidePanelAlien;
+	private EscapePicturePanel sidePanelAlien1;
+	private EscapePicturePanel sidePanelAlien2;
+	private EscapePicturePanel sidePanelAlien3;
+	private EscapePicturePanel sidePanelAlien4;
+	private EscapePicturePanel sidePanelAlien5;
+	private EscapePicturePanel sidePanelAlien6;
+	private EscapePicturePanel sidePanelAlien7;
+	private EscapePicturePanel sidePanelAlien8;
+
 	private EscapePicturePanel leftSidePanelSpace;
 	private EscapePicturePanel rightSidePanelSpace;
 	private JTextField jtfChatInput;
 	private JTextArea jtaChatRoom;
+	private JLabel jlWelcome;
+	private JPanel jpNorth;
 	
 	public EscapeChatFrame()
 	{
@@ -32,30 +43,48 @@ public class EscapeChatFrame extends JPanel implements Observer {
 	{
 		setLayout(new BorderLayout());
  		
-		sidePanelAlien = new EscapePicturePanel("Picture/greyAlienHead.png");
+		sidePanelAlien1 = new EscapePicturePanel("Picture/AlienHead.png");
+		sidePanelAlien2 = new EscapePicturePanel("Picture/AlienHead.png");
+		sidePanelAlien3 = new EscapePicturePanel("Picture/AlienHead.png");
+		sidePanelAlien4 = new EscapePicturePanel("Picture/AlienHead.png");
+		sidePanelAlien5 = new EscapePicturePanel("Picture/AlienHead.png");
+		sidePanelAlien6 = new EscapePicturePanel("Picture/AlienHead.png");
+		sidePanelAlien7 = new EscapePicturePanel("Picture/AlienHead.png");
+		sidePanelAlien8 = new EscapePicturePanel("Picture/AlienHead.png");
 		leftSidePanelSpace = new EscapePicturePanel("Picture/SpaceBackground.jpg");
 		rightSidePanelSpace = new EscapePicturePanel("Picture/SpaceBackground.jpg");
 		
 		jtfChatInput = new JTextField();
+		jtfChatInput.setPreferredSize(new Dimension(20, 40));
+
 		jtaChatRoom = new JTextArea();
+		jlWelcome = new JLabel("");
 		
 		rightSidePanelSpace.setPreferredSize(new Dimension(140, 500));
 		leftSidePanelSpace.setPreferredSize(new Dimension(140, 500));
 		jtaChatRoom.setPreferredSize(new Dimension(140, 500));
 
+		add(jlWelcome, BorderLayout.NORTH);
 		add(rightSidePanelSpace, BorderLayout.EAST);
 		add(leftSidePanelSpace, BorderLayout.WEST);
 		add(jtfChatInput, BorderLayout.SOUTH);
 		add(jtaChatRoom, BorderLayout.CENTER);
 		
-		jtfChatInput.setPreferredSize(new Dimension(20, 40));
+		rightSidePanelSpace.setLayout(new GridLayout(4, 1));
+		rightSidePanelSpace.add(sidePanelAlien1);
+		rightSidePanelSpace.add(sidePanelAlien2);
+		rightSidePanelSpace.add(sidePanelAlien3);
+		rightSidePanelSpace.add(sidePanelAlien4);
 		
+		leftSidePanelSpace.setLayout(new GridLayout(4, 1));
+		leftSidePanelSpace.add(sidePanelAlien5);
+		leftSidePanelSpace.add(sidePanelAlien6);
+		leftSidePanelSpace.add(sidePanelAlien7);
+		leftSidePanelSpace.add(sidePanelAlien8);
 	}
 	
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		
-	}
-	
-	
+	}	
 }
