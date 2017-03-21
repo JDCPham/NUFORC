55bd49dc53b848632daec11a458622925d7b4487
Emmanuel <Emmanuel@Manu>
2017-03-21 15:21:26 +0000

    Created first part of the GUI for Panel 4.
    Added text area, text fields and pictures to frame. Created class to
    conver images into JPanels.

---
+++ ppa-major-coursework/src/requirementXv2/view/EscapeChatFrame.java
@@ -0,0 +1,58 @@
+package requirementXv2.view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class EscapeChatFrame extends JPanel implements Observer {
+
+	private EscapePicturePanel sidePanelAlien;
+	private EscapePicturePanel leftSidePanelSpace;
+	private EscapePicturePanel rightSidePanelSpace;
+	private JTextField jtfChatInput;
+	private JTextArea jtaChatRoom;
+	
+	public EscapeChatFrame()
+	{
+		initWidgets();
+		setVisible(true);
+	}
+	
+	public void initWidgets()
+	{
+		setLayout(new BorderLayout());
+ 		
+		sidePanelAlien = new EscapePicturePanel("Picture/greyAlienHead.png");
+		leftSidePanelSpace = new EscapePicturePanel("Picture/SpaceBackground.jpg");
+		rightSidePanelSpace = new EscapePicturePanel("Picture/SpaceBackground.jpg");
+		
+		jtfChatInput = new JTextField();
+		jtaChatRoom = new JTextArea();
+		
+		rightSidePanelSpace.setPreferredSize(new Dimension(400, 500));
+		leftSidePanelSpace.setPreferredSize(new Dimension(400, 500));
+
+		add(rightSidePanelSpace, BorderLayout.EAST);
+		add(leftSidePanelSpace, BorderLayout.WEST);
+		add(jtfChatInput, BorderLayout.SOUTH);
+		add(jtaChatRoom, BorderLayout.CENTER);
+		
+		jtfChatInput.setPreferredSize(new Dimension(getHeight(), 40));
+		
+	}
+	
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		
+	}
+}
---
+++ ppa-major-coursework/src/requirementXv2/view/EscapePicturePanel.java
@@ -0,0 +1,30 @@
+package requirementXv2.view;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class EscapePicturePanel extends JPanel {
+	
+	private BufferedImage image;
+
+	public EscapePicturePanel(String path)
+	{
+		try {
+			
+			this.image = ImageIO.read(new File(path));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		setOpaque(false);
+	}
+	
+	protected void paintComponent(Graphics g)
+	{
+	    g.drawImage(image, 0, 0, this);
+	}
+}
