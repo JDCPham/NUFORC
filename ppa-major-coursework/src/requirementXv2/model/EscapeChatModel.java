2073d3ff7afc23ec87ce5e5939e2503c9e1e261b
caokynguyen <caokynguyen@caokys-mbp>
2017-03-23 23:07:20 +0000

    Created the game states and methods to check the game state.

--- ppa-major-coursework/src/requirementXv2/model/EscapeChatModel.java
+++ ppa-major-coursework/src/requirementXv2/model/EscapeChatModel.java
@@ -4,7 +4,31 @@ import java.util.Observable;
 
 public class EscapeChatModel extends Observable {
 	
+	// Game state
 	public static final int GAME_ACTIVE = 1;
-	public static final int GAME_LOSS = 2;
+	public static final int GAME_WIN = 2;
+	public static final int GAME_LOSS = 3;
+	
+	// Field to hold gamestate
+	private int gameState;
+	// Field to hold player score.
+	private int score;
 
+	public EscapeChatModel() {
+		
+		gameState = GAME_ACTIVE;
+		score = 0;
+	}
+	
+	public void checkGameState() {
+		
+		if ( score >= 0 && score < 4) {
+			
+			gameState = GAME_LOSS;
+		}
+		else {
+			
+			gameState = GAME_WIN;
+		}
+	}
 }
