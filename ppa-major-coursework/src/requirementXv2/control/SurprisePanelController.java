86e6c0644ddeaa48674a1466e24274d9a7b08c9c
caokynguyen <caokynguyen@10.40.216.241>
2017-03-31 13:16:04 +0100

    Added comments to the controller class for the surprise panel.

--- ppa-major-coursework/src/requirementXv2/control/SurprisePanelController.java
+++ ppa-major-coursework/src/requirementXv2/control/SurprisePanelController.java
@@ -7,79 +7,102 @@ import javax.swing.JButton;
 
 import requirementXv2.model.SurprisePanelModel;
 import requirementXv2.view.ChatRoomFrame;
-
+/**
+ * 
+ * @author caokynguyen
+ * 
+ */
 public class SurprisePanelController implements ActionListener {
 
 	private ChatRoomFrame view;
 	private SurprisePanelModel model;
 	private int questionCount;
 	private int answer;
-	
+
+	/**
+	 * When this class is created, 
+	 * @param view
+	 * @param model
+	 */
 	public SurprisePanelController(ChatRoomFrame view, SurprisePanelModel model) {
 		this.view = view;
 		this.model = model;
 		view.addActionListener(this);
-		for (int i = 0; i<5; i++) {
+		for (int i = 0; i < 5; i++) {
 			model.write(model.getAnswerPicked(0, i));
-			view.write(model.getAnswerPicked(0,i));
+			view.write(model.getAnswerPicked(0, i));
 		}
 	}
 
 	@Override
+	/**
+	 * Method to check what button the user clicked.
+	 */
 	public void actionPerformed(ActionEvent e) {
-		if (questionCount < 10 ) {
-			System.out.println("questionCount " + questionCount);
-			if (((JButton)e.getSource()).getName().equals("buttonA")) {
-				
+		if (questionCount < 10) {
+
+			if (((JButton) e.getSource()).getName().equals("buttonA")) {
+
 				view.write(model.rewrite());
 				view.write(model.getAnswerPicked(questionCount, 1));
 				model.write(model.getAnswerPicked(questionCount, 1));
-				
-				System.out.println("button A pressed");
+
 				answer = 1;
-				
+
 			}
-			if (((JButton)e.getSource()).getName().equals("buttonB")) {
-				
+			if (((JButton) e.getSource()).getName().equals("buttonB")) {
+
 				view.write(model.rewrite());
 				view.write(model.getAnswerPicked(questionCount, 2));
 				model.write(model.getAnswerPicked(questionCount, 2));
-				
-				System.out.println("button B pressed");
+
 				answer = 2;
 			}
-			if (((JButton)e.getSource()).getName().equals("buttonC")) {
-				
+			if (((JButton) e.getSource()).getName().equals("buttonC")) {
+
 				view.write(model.rewrite());
 				view.write(model.getAnswerPicked(questionCount, 3));
 				model.write(model.getAnswerPicked(questionCount, 3));
-				System.out.println("button C pressed");
+
 				answer = 3;
-				
+
 			}
-			if (((JButton)e.getSource()).getName().equals("buttonD")) {
-				
+			if (((JButton) e.getSource()).getName().equals("buttonD")) {
+
 				view.write(model.rewrite());
 				view.write(model.getAnswerPicked(questionCount, 4));
 				model.write(model.getAnswerPicked(questionCount, 4));
-				System.out.println("button D pressed");
+
 				answer = 4;
-		
+
 			}
 			model.checkAnswer(questionCount, answer);
-			System.out.println(model.getScore());
+
 			questionCount++;
 			if (questionCount < 10) {
 				showQuestion();
+			} else {
+				if (model.getScore() > 6) {
+					view.write("Congratulations you have won!");
+				}else{
+					view.write("You have lost!");
+				}
+				
+				view.write("<html><b>MR STEAL YOUR GIRL AKA MARTIN CHAPMAN HAS STOLEN YOUR GIRL</b></html>");
+				view.write("<html><b>BETTER LUCK NEXT TIME</b></html>Ï");
+
 			}
-		}	
-	}
-	
-	public void showQuestion() {
-		for (int i = 0; i<5; i++) {
-			model.write(model.getAnswerPicked(questionCount, i));
-			view.write(model.getAnswerPicked(questionCount,i));
 		}
 	}
 	
+	/**
+	 * 
+	 */
+	public void showQuestion() {
+		for (int i = 0; i < 5; i++) {
+			model.write(model.getAnswerPicked(questionCount, i));
+			view.write(model.getAnswerPicked(questionCount, i));
+		}
+	}
+
 }
